b2f743dce631a46eb9b57d97a996c3ca641625e4
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-02-03 04:19:57 -0600

    login table added

--- src/main/java/com/example/University/Portal/Database_Connection/LoginInfo.java
+++ src/main/java/com/example/University/Portal/Database_Connection/LoginInfo.java
@@ -13,16 +13,47 @@ public class LoginInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column()
     @Id
-    private int Id;
-    @Column(unique =  true)
+    private int userId;
+    @Column(unique = true)
     private String email;
     @Column()
     private String password;
     @Column()
     private String role;
 
+    LoginInfo() {
+    }
 
-    LoginInfo(){}
-    LoginInfo(String email, String password){}
+    LoginInfo(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getter And Setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
 }
