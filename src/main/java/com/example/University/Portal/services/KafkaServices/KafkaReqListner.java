e654eef89b069a877fc46a766c5ec90c8c588222
AbdulRafay-00 <iamrafay.00@gmail.com>
2026-03-18 12:02:38 -0500

    consumer was also added

--- src/main/java/com/example/University/Portal/services/KafkaServices/KafkaReqListner.java
+++ src/main/java/com/example/University/Portal/services/KafkaServices/KafkaReqListner.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaReqListner {
     
-    @KafkaListener(topics = "ai-response-topic", groupId = "portal-group")
+    @KafkaListener(topics = "ai-response-topic", groupId = "university-group")
     public void receiveResponse(String response) {
     System.out.println("AI Response: " + response);
 }
